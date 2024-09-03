e2e93c98822dc7f42065eebcaa8ec3d197a9823d
pivnati <natanmuleta911@gmail.com>
2024-09-03 13:47:24 -0700

    add the instagram images in the head line and make it center

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,23 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <div class="d-flex flex-column align-items-center mr-5">
-        <div style="max-width: 50px; height:auto; margin-bottom: 8px;  margin-left:10px;">
-            <img src="https://c8.alamy.com/comp/2RGT4W0/piv-letter-logo-design-with-polygon-shape-piv-polygon-and-cube-shape-logo-design-piv-hexagon-vector-logo-template-white-and-black-colors-piv-monogr-2RGT4W0.jpg" alt="">
+
+    <div class="navbar-brand d-flex justify-content-between align-items-center w-100" href="{{url('/')}}">
+        <div class="d-flex align-items-center">
+            <div class="me-2" style="max-width: 50px;">
+                <img src="https://c8.alamy.com/comp/2RGT4W0/piv-letter-logo-design-with-polygon-shape-piv-polygon-and-cube-shape-logo-design-piv-hexagon-vector-logo-template-white-and-black-colors-piv-monogr-2RGT4W0.jpg" alt="" class="img-fluid">
+            </div >
+            <div class="fw-bold" style="font-family: 'Poppins', sans-serif;" class="margin-left:40px;">PIV IG</div>
         </div>
-        <div style="font-family: 'Poppins', sans-serif; font-weight: bold; margin-left:10px;">PIV IG</div>
+
+        <!-- Another image on the right side -->
+        <div class="d-flex mb-2 justify-content-center align-items-center me-2" style="max-width: 50px; margin-left:700px; margin-bottom: 10px;">
+            <img src="https://thumbs.dreamstime.com/b/instagram-new-logo-chisinau-moldova-september-instagram-new-logo-printed-white-paper-instagram-online-mobile-photo-128373447.jpg" alt="" class="img-fluid">
+        </div>
 
     </div>
 
-
-
     <!-- Fonts -->
-
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
