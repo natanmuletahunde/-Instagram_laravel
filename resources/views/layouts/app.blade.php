9e9d91f8f00dbe948f4de590544abdb53d4208c2
pivnati <natanmuleta911@gmail.com>
2024-09-03 13:23:06 -0700

    make both the image and the piv ig text margin-to-letf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="d-flex flex-column align-items-center mr-5">
-        <div style="max-width: 50px; height:auto; margin-bottom: 8px;">
+        <div style="max-width: 50px; height:auto; margin-bottom: 8px;  margin-left:10px;">
             <img src="https://c8.alamy.com/comp/2RGT4W0/piv-letter-logo-design-with-polygon-shape-piv-polygon-and-cube-shape-logo-design-piv-hexagon-vector-logo-template-white-and-black-colors-piv-monogr-2RGT4W0.jpg" alt="">
         </div>
-        <div>PIV IG</div>
+        <div style="font-family: 'Poppins', sans-serif; font-weight: bold; margin-left:10px;">PIV IG</div>
+
     </div>
 
 
