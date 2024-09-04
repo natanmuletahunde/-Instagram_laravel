be72e877d105ac924037ce1a7f51355061949cb0
pivnati <natanmuleta911@gmail.com>
2024-09-03 18:53:48 -0700

    add the images in the nav bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
        <div class="row">
         <div class="col-3">
-
+          <img src="https://c8.alamy.com/comp/M5BHFC/pivx-blockchain-cryptocurrency-coin-vector-logo-of-pivx-M5BHFC.jpg" alt="">
         </div>
           <div class="col-9">
             
