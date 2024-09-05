f5a8c72ca84f3c2981d252d1099609a3d6ba04eb
pivnati <natanmuleta911@gmail.com>
2024-09-05 08:33:35 -0700

    install and import bootstrap in our app.css

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,16 @@
 @section('content')
-<div class="container mx-auto">
-    <div class="flex">
-        <!-- Image on the left side -->
-        <div class="w-1/4 p-2">
-            <img src="https://c8.alamy.com/comp/M5BHFC/pivx-blockchain-cryptocurrency-coin-vector-logo-of-pivx-M5BHFC.jpg" alt="" class="w-full h-auto">
+<div class="container">
+       <div class="row">
+        <div class="col-3 p-5 ">
+        <img src="https://c8.alamy.com/comp/M5BHFC/pivx-blockchain-cryptocurrency-coin-vector-logo-of-pivx-M5BHFC.jpg" class="w-32 h-32" alt="">
         </div>
-        <!-- Content on the right side -->
-        <div class="w-3/4 p-2">
-            <!-- Your content here -->
-        </div>
-    </div>
+          <div class="col-9">
+            <div> <h1> piv ig profile</h1></div>
+            <div>
+             <div> <strong>153</strong></div>
+             <div><strong>23k</strong>followers</div>
+             <div> <strong>212</strong>following</div>
+             </div>
+          </div>
+       </div> 
 </div>
-@endsection
