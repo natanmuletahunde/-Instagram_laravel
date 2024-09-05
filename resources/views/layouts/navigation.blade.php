4501d9fe756d4dd12b54ec100ef6f7906ffef4f2
pivnati <natanmuleta911@gmail.com>
2024-09-04 21:47:30 -0700

    update the navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,14 @@
 @section('content')
-<div class="container">
-       <div class="row">
-        <div class="col-3">
-          <img src="https://c8.alamy.com/comp/M5BHFC/pivx-blockchain-cryptocurrency-coin-vector-logo-of-pivx-M5BHFC.jpg" alt="">
+<div class="container mx-auto">
+    <div class="flex">
+        <!-- Image on the left side -->
+        <div class="w-1/4 p-2">
+            <img src="https://c8.alamy.com/comp/M5BHFC/pivx-blockchain-cryptocurrency-coin-vector-logo-of-pivx-M5BHFC.jpg" alt="" class="w-full h-auto">
         </div>
-          <div class="col-9">
-            
-          </div>
-       </div> 
+        <!-- Content on the right side -->
+        <div class="w-3/4 p-2">
+            <!-- Your content here -->
+        </div>
+    </div>
 </div>
 @endsection
